dd89725833353f439ca48b2eeb83e041fe494bdf
Adam Pawelek <adam.pwk@outlook.com>
2023-07-31 23:51:06 +0200

    Added new post route

--- go_postgres/handlers/facts.go
+++ go_postgres/handlers/facts.go
@@ -10,9 +10,15 @@ func Home(c *fiber.Ctx) error {
 	return c.SendString("Hello, World!")
 }
 
+func ListFacts(c *fiber.Ctx) error {
+	facts := []models.Fact{}
+	database.DB.Db.Find(&facts)
+
+	return c.Status(200).JSON(facts)
+}
+
 func CreateFact(c *fiber.Ctx) error {
 	fact := new(models.Fact)
-
 	if err := c.BodyParser(fact); err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"message": err.Error(),
