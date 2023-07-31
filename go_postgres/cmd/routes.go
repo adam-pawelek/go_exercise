4713c133c51ad4528ad832649197e635b507b0b0
Adam Pawelek <adam.pwk@outlook.com>
2023-07-31 23:19:30 +0200

    next changes

--- go_postgres/cmd/routes.go
+++ go_postgres/cmd/routes.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"github.com/adam-pawelek/go_exercise/blob/main/go_postgres/handlers"
+	"github.com/adam-pawelek/go_exercise/tree/main/go_postgres/handlers"
 	"github.com/gofiber/fiber/v2"
 )
 
 func setupRoutes(app *fiber.App) {
-	app.Get("/", handlers.Home(c))
+	app.Get("/", handlers.Home)
 }
